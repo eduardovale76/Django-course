4676a5a507f10c660ab2e6a3fe4eef4e734f1d3d
Eduardo Vale <eduardovale76@gmail.com>
2022-04-17 17:00:54 -0300

    Sentry set up

--- pypro/settings.py
+++ pypro/settings.py
@@ -185,4 +185,11 @@ if AWS_ACCESS_KEY_ID:
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')
     
-    
+import sentry_sdk    
+from sentry_sdk.integrations.django import DjangoIntegration
+
+SENTRY_DNS = config('SENTRY_DNS', default=None)
+
+if SENTRY_DNS:
+    sentry_sdk.init(
+    dsn=SENTRY_DNS,integrations=[DjangoIntegration()],traces_sample_rate=1.0,send_default_pii=True)
