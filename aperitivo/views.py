e78ae90c6b5f8594cb367b8dd02ef8011ed4caa4
Eduardo Vale <eduardovale76@gmail.com>
2022-04-25 19:51:57 -0300

    Template's filter

--- aperitivo/views.py
+++ aperitivo/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 
 
 def video(request,slug):
-    video = {
-    'titulo':'Video Aperitivo: Motivação',
-    'youtube_id':'23tusPiiNZk',
-}
+    
+    videos = {
+    'motivacao': {'titulo':'Video Aperitivo: Motivação', 'youtube_id':'23tusPiiNZk'},
+    'instalacao-windows': {'titulo':'Instalação Windows', 'youtube_id':'QZ1ASyiq-NQ'},
+    }
+    
+    video = videos[slug]
+    
     return render(request, 'aperitivo/video.html', context={'video':video})
