b065ded6f695046823e6046e905945e82eb0a0ae
Eduardo Vale <eduardovale76@gmail.com>
2022-05-11 20:47:43 -0300

    Class details page

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,4 +1,4 @@
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Modulo, Aula
 from typing import List
 
 def lista_modulos_ordenados() -> List[Modulo]:
@@ -15,3 +15,6 @@ def encontrar_modulo(slug: str) -> Modulo:
 
 def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
     return list(modulo.aula_set.order_by('order').all())
+
+def encontrar_aula(slug):
+    return Aula.objects.get(slug=slug)
