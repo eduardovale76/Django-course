9459c8998bf59a750ab452af4cf8174e29b3049f
Eduardo Vale <eduardovale76@gmail.com>
2022-06-20 19:49:48 -0300

    Decorator Login Required

--- base/tests/test_login.py
+++ base/tests/test_login.py
@@ -54,3 +54,6 @@ def test_botao_sair_indisponivel(resp_home_com_usuario_logado):
     
 def test_nome_usuario_logado_disponivel(resp_home_com_usuario_logado, usuario_logado):
     assert_contains(resp_home_com_usuario_logado, usuario_logado.first_name)
+    
+def test_link_logout_disponivel(resp_home_com_usuario_logado):
+    assert_contains(resp_home_com_usuario_logado, reverse('logout'))
--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from model_bakery import baker
 from pypro.django_assertions import assert_contains
 from pypro.modulos.models import Modulo, Aula
-
+from base.conftest import client_com_usuario_logado, usuario_logado
 
 @pytest.fixture
 def modulo(db):
@@ -14,8 +14,8 @@ def aula(modulo):
     return baker.make(Aula, modulo=modulo)
 
 @pytest.fixture
-def resp(client, aula):
-    resp = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+def resp(client_com_usuario_logado, aula):
+    resp = client_com_usuario_logado.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
     return resp
 
 def test_titulo(resp, aula: Aula):
@@ -29,3 +29,11 @@ def test_youtube(resp, aula: Aula):
 def test_modulo_breadcrumb(resp, modulo: Modulo):
     assert_contains(resp,f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{modulo.titulo}</a></li>')
  
+@pytest.fixture
+def resp_sem_usuario(client, aula):
+    resp = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+    return resp
+
+def test_usuario_nao_logado_redirect(resp_sem_usuario):
+    assert resp_sem_usuario.status_code == 302
+    assert resp_sem_usuario.url.startswith(reverse('login'))
